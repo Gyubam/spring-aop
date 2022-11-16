097cfc12386a0d0adfd427d1cdacac257e77cbc2
sgb8170@naver.com <sgb8170@naver.com>
2022-11-17 00:54:15 +0900

    여러 어드바이스 적용

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -20,7 +20,8 @@ import org.springframework.stereotype.Component;
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
